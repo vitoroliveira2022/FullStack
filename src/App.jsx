12f56c80ffb866bc39d8981f262f5ae04e7026bc
vitoroliveira2022 <vitor.oliveira.2001@hotmail.com>
2023-10-18 21:08:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ApiAnime from './componentes/ApiAnime';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá bem vindo a minha pagina !</h1>
+     <ApiAnime></ApiAnime>
+    </div>
+  );
+}
+
+export default App;
